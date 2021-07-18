10078ee2474d71af238d3c3d35a2bc2ff59027ff
湫曗 <autumnsilverleaves@gmail.com>
2021-07-19 02:56:21 +0800

    calc size

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,7 @@ import { onMounted, reactive } from "vue";
 import { defineFunctionComponent } from "./support/defineFunctionComponent";
 
 export const Contain = defineFunctionComponent((a, { slots }) => {
-  const w = (
-    <div
-      class=" inline-block w-full opacity-40 absolute"
-      style={{
-        aspectRatio: (16 / 9).toString(),
-      }}
-    ></div>
-  );
-
-  const h = (
-    <div
-      class=" inline-block h-full opacity-40 absolute"
-      style={{
-        aspectRatio: (16 / 9).toString(),
-      }}
-    ></div>
-  );
+  const ratio = 16 / 9;
 
   const size = reactive({
     width: 0,
@@ -26,30 +10,35 @@ export const Contain = defineFunctionComponent((a, { slots }) => {
   });
 
   const container = (
-    <div class="bg-gray-400 absolute left-0 right-0 top-0 bottom-0 -z-1">
-      {[w, h]}
-    </div>
+    <div class="bg-gray-400 absolute left-0 right-0 top-0 bottom-0 -z-1"></div>
   );
 
   const observe = new ResizeObserver((event) => {
     const containerElement = container.el as HTMLDivElement;
-    const wElement = w.el as HTMLDivElement;
-    const hElement = h.el as HTMLDivElement;
+    const wSize = {
+      clientWidth: containerElement.clientWidth,
+      clientHeight: containerElement.clientWidth / ratio,
+    };
+    const hSize = {
+      clientWidth: containerElement.clientHeight * ratio,
+      clientHeight: containerElement.clientHeight,
+    };
+    const wElement = wSize;
+    const hElement = hSize;
 
     if (wElement.clientHeight <= containerElement.clientHeight) {
       size.width = wElement.clientWidth;
       size.height = wElement.clientHeight;
-      console.log();
     } else {
       size.width = hElement.clientWidth;
       size.height = hElement.clientHeight;
     }
-    console.log(
-      "resize",
-      JSON.stringify(size, undefined, 4),
-      size.width / size.height,
-      16 / 9
-    );
+    // console.log(
+    //   "resize",
+    //   JSON.stringify(size, undefined, 4),
+    //   size.width / size.height,
+    //   16 / 9
+    // );
   });
 
   onMounted(() => {
